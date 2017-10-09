3b0a89770a91c7744809b0a4d5341c856b3df7dc
Carmen María Soto <carsoto8691@gmail.com>
2017-10-09 15:07:29 -0400

    [Capitulo 3] - Mostrar listado de colección de objetos utilizando v-for

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ new Vue({
 		texto: "Hola Mundo desde VueJS 2",
 		nombre: "Nombre por defecto",
 		nota: 5,
-		peliculas: ['Batman vs Superman', 'La verdad duele', 'Los mercenarios', 'Spiderman 3', 'Titanic']
+		peliculas: ['Batman vs Superman', 'La verdad duele', 'Los mercenarios', 'Spiderman 3', 'Titanic'],
+		frutas: [
+			{nombre: 'Manzana', temporada: 'Invierno', precio: 'Bajo'},
+			{nombre: 'Naranja', temporada: 'Otoño', precio: 'Medio'},
+			{nombre: 'Cereza', temporada: 'Primavera', precio: 'Alto'},
+			{nombre: 'Sandía', temporada: 'Verano', precio: 'Medio'}
+		]
 	}
 });
