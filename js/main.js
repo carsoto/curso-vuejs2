23e272e7000d47ad09f7e7bf3dc1527fe0e36c28
Carmen María Soto <carsoto8691@gmail.com>
2017-10-09 14:51:09 -0400

    [Capitulo 3] - Directivas de iteración (v-for)

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ new Vue({
 	data: {
 		texto: "Hola Mundo desde VueJS 2",
 		nombre: "Nombre por defecto",
-		nota: 5
+		nota: 5,
+		peliculas: ['Batman vs Superman', 'La verdad duele', 'Los mercenarios', 'Spiderman 3', 'Titanic']
 	}
 });
